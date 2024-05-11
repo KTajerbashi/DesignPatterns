0fd506199b1ea159dcc4ed60fa8978ae9b3e556c
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-05-12 00:29:44 +0330

    Feature : Changed to Singleton Service

--- Program.cs
+++ Program.cs
@@ -1,3 +1,64 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using DesignPatterns.Factory.Services;
+using DesignPatterns.Tools;
+
+DesignConsole console = DesignConsole.DesignInstance();
+
+
+console.Start("Application");
+#region ProtoType
+//#region ShalowCopy
+//ConcreatePrototype ShalowCopy1 = new ConcreatePrototype(1500,"Prototype Pattern");
+
+//ConcreatePrototype ShalowCopy2 = (ConcreatePrototype)ShalowCopy1.Clone();
+
+//Console.WriteLine($"Service1 : {ShalowCopy1.Size}");
+//Console.WriteLine($"Service2 : {ShalowCopy2.Size}");
+//Console.WriteLine($"Result Equal : {ShalowCopy1 == ShalowCopy2}");
+//#endregion
+
+//#region DeepCopy
+//ConcreatePrototype2 DeepCopyComplex1 = new ConcreatePrototype2(ShalowCopy1);
+//var DeepCopyComplex2 = (ConcreatePrototype2)DeepCopyComplex1.Clone();
+
+//Console.WriteLine($"Service1 : {DeepCopyComplex1.prototype1.Size}");
+//Console.WriteLine($"Service1 : {DeepCopyComplex1.prototype1.Name}");
+//Console.WriteLine($"Service2 : {DeepCopyComplex2.prototype1.Size}");
+//Console.WriteLine($"Service2 : {DeepCopyComplex2.prototype1.Name}");
+//Console.WriteLine($"Result Equal : {DeepCopyComplex1 == DeepCopyComplex2}");
+//Console.WriteLine($"Result Equal : {DeepCopyComplex1 == DeepCopyComplex1}");
+
+//#endregion
+#endregion
+
+#region Singleton
+
+//Singleton singleton1 = Singleton.GetInstance();
+//Singleton singleton2 = Singleton.GetInstance();
+//Singleton singleton3 = Singleton.GetInstance();
+//for (int i = 0; i < 10; i++)
+//{
+//    Singleton.GetInstance();
+//}
+//Console.WriteLine(singleton1 == singleton2);
+
+#endregion
+
+#region Factory
+ConcreteCreator concreteCreator = new ConcreteCreator();
+ISmsService mailService;
+mailService = concreteCreator.FactoryMethod(CountryType.None);
+mailService.Send(new MailDTO { Phone="09020320844",Email="Kaihan@mail.com",Message="Hello"});
+mailService.GetMails();
+mailService = concreteCreator.FactoryMethod(CountryType.Afghanistan);
+mailService.Send(new MailDTO { Phone = "09024006014", Email = "Kamran@mail.com", Message = "Hello" });
+mailService = concreteCreator.FactoryMethod(CountryType.Iran);
+mailService.Send(new MailDTO { Phone = "09961514027", Email = "Tajer@mail.com", Message = "Hello" });
+mailService = concreteCreator.FactoryMethod(CountryType.Turkey);
+mailService.Send(new MailDTO { Phone = "09021001000", Email = "Tiger@mail.com", Message = "Hello" });
+#endregion
+
+console.End("Application");
+
+
 Console.ReadLine();
---
+++ Tools/DesignConsole.cs
@@ -0,0 +1,61 @@
+﻿namespace DesignPatterns.Tools
+{
+    public class DesignConsole
+    {
+        private static readonly  Lazy<DesignConsole> Design = new Lazy<DesignConsole>(() => new DesignConsole());
+        private DesignConsole()
+        {
+            Console.Title = "Design Pattern";
+        }
+        
+        public static DesignConsole DesignInstance()
+        {
+            return Design.Value;
+        }
+
+
+
+        public void Background(ConsoleColor color)
+        {
+            Console.BackgroundColor = color;
+        }
+
+        public void ForeColor(ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+        }
+
+        public void NewLine()
+        {
+            Console.WriteLine("\n");
+        }
+        public void Section()
+        {
+            Console.WriteLine("\n:::::::::::::::::::::::::::::::::::::::::::::::::::::::\n");
+        }
+        public void Title(string title)
+        {
+            Console.WriteLine($"\n== == == == == == {title} == == == == == ==\n");
+        }
+        public void Start(string title)
+        {
+            ForeColor(ConsoleColor.Black);
+            Background(ConsoleColor.Red);
+            Console.WriteLine("======================================================");
+            Console.WriteLine($"== == == == Start Of {title} \t");
+            Console.WriteLine("======================================================");
+            Background(ConsoleColor.Black);
+        }
+        public void End(string title)
+        {
+            ForeColor(ConsoleColor.Black);
+            Background(ConsoleColor.Red);
+            Console.WriteLine("======================================================");
+            Console.WriteLine($"== == == == End Of {title} \t");
+            Console.WriteLine("======================================================");
+            Background(ConsoleColor.Black);
+            Section();
+        }
+    }
+
+}
