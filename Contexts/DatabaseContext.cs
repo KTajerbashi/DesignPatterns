45b1539ff56225f683e14b83715694b7d132c736
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-05-18 23:04:42 +0330

    Fix : Change Namespaces

--- Contexts/DatabaseContext.cs
+++ Contexts/DatabaseContext.cs
@@ -1,4 +1,4 @@
-﻿using DesignPatterns.Composite.Categories;
+﻿using DesignPatterns.Composite.Examples.Categories;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
