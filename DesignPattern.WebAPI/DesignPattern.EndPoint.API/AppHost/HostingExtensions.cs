0c0de5523c0e11b6c95e553c0aadc55a22fcfaf6
Tajerbashi <kamrantajerbashi@gmail.com>
2024-09-09 18:55:17 +0330

    Feature : Create Hosted Program Application

---
+++ DesignPattern.WebAPI/DesignPattern.EndPoint.API/AppHost/HostingExtensions.cs
@@ -0,0 +1,60 @@
+﻿using DataAccesses.Contexts.CommandsContextDatabase;
+using DataAccesses.Contexts.QueriesContextDatabase;
+using DesignPattern.EndPoint.API.DIContainer;
+using DesignPattern.EndPoint.API.Middlewares;
+using Microsoft.EntityFrameworkCore;
+using NLog.Extensions.Logging;
+using NLog.Web;
+
+namespace DesignPattern.EndPoint.API.AppHost;
+
+public static class HostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        var Configuration = builder.Configuration;
+
+        builder.Services.AddDbContext<CommandsDatabaseContext>(options =>
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        });
+        builder.Services.AddDbContext<QueriesDatabaseContext>(options =>
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        });
+        builder.Services.AddPatterServices();
+
+        //configure logging
+        builder.Services.AddLogging(loggingBuilder =>
+        {
+            loggingBuilder.ClearProviders();
+            loggingBuilder.AddNLog().SetMinimumLevel(LogLevel.Trace);
+            //loggingBuilder.AddConsole().SetMinimumLevel(LogLevel.Trace);
+        });
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        return builder.Build();
+    }
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        app.UseMyMiddleware();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
+}
--- DesignPattern.WebAPI/DesignPattern.EndPoint.API/Program.cs
+++ DesignPattern.WebAPI/DesignPattern.EndPoint.API/Program.cs
@@ -1,67 +1,14 @@
-using DataAccesses.Contexts.CommandsContextDatabase;
-using DataAccesses.Contexts.QueriesContextDatabase;
-using DesignPattern.EndPoint.API.DIContainer;
-using DesignPattern.EndPoint.API.Middlewares;
-using Microsoft.EntityFrameworkCore;
-using NLog.Extensions.Logging;
-using NLog.Web;
-var logger = NLogBuilder.ConfigureNLog("NLog.config").GetCurrentClassLogger();
+using DesignPattern.EndPoint.API.AppHost;
 
-try
+StartApplication.RunApplication(() =>
 {
-
     var builder = WebApplication.CreateBuilder(args);
-    var Configuration = builder.Configuration;
+    builder
+    .ConfigureServices()
+    .ConfigurePipeline()
+    .Run();
+});
 
-    builder.Services.AddDbContext<CommandsDatabaseContext>(options =>
-    {
-        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionCommand"));
-    });
-    builder.Services.AddDbContext<QueriesDatabaseContext>(options =>
-    {
-        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionQuery"));
-    });
 
-    builder.Services.AddPatterServices();
 
-    //configure logging
-    builder.Services.AddLogging(loggingBuilder =>
-    {
-        loggingBuilder.ClearProviders();
-        loggingBuilder.AddNLog().SetMinimumLevel(LogLevel.Trace);
-        //loggingBuilder.AddConsole().SetMinimumLevel(LogLevel.Trace);
-    });
 
-    // Add services to the container.
-    builder.Services.AddControllers();
-
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-
-    var app = builder.Build();
-    // Configure the HTTP request pipeline.
-    app.UseMyMiddleware();
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-    app.UseHttpsRedirection();
-
-    app.UseAuthorization();
-
-    app.MapControllers();
-
-    app.Run();
-
-}
-catch (Exception ex)
-{
-    logger.Error(ex, "Stopped program because of exception");
-    throw;
-}
-finally
-{
-    NLog.LogManager.Shutdown();
-}
