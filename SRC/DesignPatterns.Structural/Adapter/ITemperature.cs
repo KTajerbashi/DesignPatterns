1a7612e10c4a14c49bd5d4de244bda357efb3567
KTajerbashi <kamrantajerbashi@gmail.com>
2025-10-21 17:07:28 +0330

    #FEature Add Some Files and NavLink

---
+++ SRC/DesignPatterns.Structural/Adapter/ITemperature.cs
@@ -0,0 +1,27 @@
+﻿namespace DesignPatterns.Structural.Adapter;
+
+public interface ITemperature
+{
+    double GetTemperatureFahrenheit();
+}
+public class CelsiusService
+{
+    public double GetTemperatureCelsius() => 25.0; // example temperature
+}
+
+public class TemperatureAdapter : ITemperature
+{
+    private readonly CelsiusService _celsiusService;
+
+    public TemperatureAdapter(CelsiusService service)
+    {
+        _celsiusService = service;
+    }
+
+    public double GetTemperatureFahrenheit()
+    {
+        return _celsiusService.GetTemperatureCelsius() * 9 / 5 + 32;
+    }
+}
+
+
