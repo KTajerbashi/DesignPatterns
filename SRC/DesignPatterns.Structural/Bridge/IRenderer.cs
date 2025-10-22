8abe4e8365693c790e25efd133483c474de39aad
KTajerbashi <kamrantajerbashi@gmail.com>
2025-10-22 15:44:24 +0330

    #Feature Add Bridge Design Pattern

---
+++ SRC/DesignPatterns.Structural/Bridge/IRenderer.cs
@@ -0,0 +1,42 @@
+﻿namespace DesignPatterns.Structural.Bridge;
+
+// Implementor
+public interface IRenderer
+{
+    string Render(string shapeName);
+}
+public class VectorRenderer : IRenderer
+{
+    public string Render(string shapeName) => $"Drawing {shapeName} as VECTORS.";
+}
+public class RasterRenderer : IRenderer
+{
+    public string Render(string shapeName) => $"Drawing {shapeName} as PIXELS.";
+}
+// Abstraction
+public abstract class Shape
+{
+    protected readonly IRenderer Renderer;
+
+    protected Shape(IRenderer renderer)
+    {
+        Renderer = renderer;
+    }
+
+    public abstract string Draw();
+}
+
+public class Circle : Shape
+{
+    public Circle(IRenderer renderer) : base(renderer) { }
+
+    public override string Draw() => Renderer.Render("Circle");
+}
+public class Square : Shape
+{
+    public Square(IRenderer renderer) : base(renderer) { }
+
+    public override string Draw() => Renderer.Render("Square");
+}
+
+
