f9b423a4772902da3989b13052cd5c0d544a640f
KTajerbashi <kamrantajerbashi@gmail.com>
2025-10-22 15:46:33 +0330

    #Feature  Add Decorator Design Pattern

---
+++ SRC/DesignPatterns.Structural/Decorator/ICoffee.cs
@@ -0,0 +1,42 @@
+﻿namespace DesignPatterns.Structural.Decorator;
+
+// The Component interface
+public interface ICoffee
+{
+    string GetDescription();
+    double GetCost();
+}
+// The Concrete Component
+public class SimpleCoffee : ICoffee
+{
+    public string GetDescription() => "Simple Coffee";
+    public double GetCost() => 2.0;
+}
+// The Base Decorator
+public abstract class CoffeeDecorator : ICoffee
+{
+    protected readonly ICoffee _coffee;
+
+    protected CoffeeDecorator(ICoffee coffee)
+    {
+        _coffee = coffee;
+    }
+
+    public virtual string GetDescription() => _coffee.GetDescription();
+    public virtual double GetCost() => _coffee.GetCost();
+}
+
+public class MilkDecorator : CoffeeDecorator
+{
+    public MilkDecorator(ICoffee coffee) : base(coffee) { }
+
+    public override string GetDescription() => $"{_coffee.GetDescription()}, Milk";
+    public override double GetCost() => _coffee.GetCost() + 0.5;
+}
+public class SugarDecorator : CoffeeDecorator
+{
+    public SugarDecorator(ICoffee coffee) : base(coffee) { }
+
+    public override string GetDescription() => $"{_coffee.GetDescription()}, Sugar";
+    public override double GetCost() => _coffee.GetCost() + 0.2;
+}
