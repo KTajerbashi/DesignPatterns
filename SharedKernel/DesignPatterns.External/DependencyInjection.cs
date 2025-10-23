be9eb14f26f6e1d71025e5677c5800880af92bb4
KTajerbashi <kamrantajerbashi@gmail.com>
2025-10-23 14:46:24 +0330

    #Feature Add External Service

---
+++ SharedKernel/DesignPatterns.External/DependencyInjection.cs
@@ -0,0 +1,8 @@
+﻿namespace DesignPatterns.External;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddServiceContainers(this IServiceCollection service)
+    {
+        return service;
+    }
+}
