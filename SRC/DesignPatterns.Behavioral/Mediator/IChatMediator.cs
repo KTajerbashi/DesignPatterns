94b7459ae5de8ade0bc76302118e3be7793e9786
KTajerbashi <kamrantajerbashi@gmail.com>
2025-10-23 14:14:12 +0330

    #Feature Add Mediator Design Pattern

---
+++ SRC/DesignPatterns.Behavioral/Mediator/IChatMediator.cs
@@ -0,0 +1,55 @@
+﻿namespace DesignPatterns.Behavioral.Mediator;
+
+public interface IUser
+{
+    string Name { get; }
+    void Send(string message);
+    void Receive(string message, string sender);
+}
+public class User : IUser
+{
+    private readonly IChatMediator _mediator;
+    public string Name { get; }
+
+    public List<string> Messages { get; } = new();
+
+    public User(string name, IChatMediator mediator)
+    {
+        Name = name;
+        _mediator = mediator;
+    }
+
+    public void Send(string message)
+    {
+        _mediator.SendMessage(message, this);
+    }
+
+    public void Receive(string message, string sender)
+    {
+        Messages.Add($"{sender}: {message}");
+    }
+}
+
+public interface IChatMediator
+{
+    void SendMessage(string message, IUser sender);
+    void AddUser(IUser user);
+}
+public class ChatMediator : IChatMediator
+{
+    private readonly List<IUser> _users = new();
+
+    public void AddUser(IUser user)
+    {
+        _users.Add(user);
+    }
+
+    public void SendMessage(string message, IUser sender)
+    {
+        foreach (var user in _users)
+        {
+            if (user != sender)
+                user.Receive(message, sender.Name);
+        }
+    }
+}
