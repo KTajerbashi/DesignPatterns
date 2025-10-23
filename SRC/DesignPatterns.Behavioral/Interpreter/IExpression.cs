5fe2d4fc7eb0b7e5396f8fd277608da8f46d949b
KTajerbashi <kamrantajerbashi@gmail.com>
2025-10-23 13:38:31 +0330

    #Feature Add Interpreter Design Pattern

---
+++ SRC/DesignPatterns.Behavioral/Interpreter/IExpression.cs
@@ -0,0 +1,45 @@
+﻿namespace DesignPatterns.Behavioral.Interpreter;
+
+public interface IExpression
+{
+    int Interpret();
+}
+public class NumberExpression : IExpression
+{
+    private readonly int _number;
+
+    public NumberExpression(int number)
+    {
+        _number = number;
+    }
+
+    public int Interpret() => _number;
+}
+
+public class AddExpression : IExpression
+{
+    private readonly IExpression _left;
+    private readonly IExpression _right;
+
+    public AddExpression(IExpression left, IExpression right)
+    {
+        _left = left;
+        _right = right;
+    }
+
+    public int Interpret() => _left.Interpret() + _right.Interpret();
+}
+
+public class SubtractExpression : IExpression
+{
+    private readonly IExpression _left;
+    private readonly IExpression _right;
+
+    public SubtractExpression(IExpression left, IExpression right)
+    {
+        _left = left;
+        _right = right;
+    }
+
+    public int Interpret() => _left.Interpret() - _right.Interpret();
+}
